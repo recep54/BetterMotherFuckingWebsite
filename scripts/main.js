23993f1161ba86db4794a4b8ddb948c8475aa43c
recep54 <43546219+recep54@users.noreply.github.com>
2018-09-26 15:33:58 +0000

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+setTimeout(function(){
+confirm('Hey, le site xxxvidsxxx est trop bien. Viens dessus !');
+}, 2000);
+
+var myButton = document.querySelector('button');
+
+
+myButton.onclick = function (){
+		document.querySelector('body').style.background = "magenta",
+		document.querySelector('button').innerHTML = 'Mode normal';
+
+myButton.onclick = function () {
+		document.querySelector('body').style.background = "#FAFAFA";
+		document.querySelector('button').innerHTML = 'Mode psychédélique';
+
+} 
+} 
